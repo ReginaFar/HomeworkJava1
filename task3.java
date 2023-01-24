53b78be0f9312e4d13e385ca74d810ae4cd84be1
Regina Shakirova <reginafar22@rambler.ru>
2023-01-24 21:09:35 +0400

    решила 3 задачу

--- task3.java
+++ task3.java
@@ -15,7 +15,24 @@ import java.util.Scanner;
 public class task3 {
     public static void main(String[] args) throws Exception {
         Scanner iScanner = new Scanner(System.in);
-        System.out.printf("Введите число 1: ");
-        int a = iScanner.nextInt();
-        System.out.println(n);
+        System.out.println("Введите два числа: ");
+        int num1 = iScanner.nextInt();
+        int num2 = iScanner.nextInt();
+        System.out.println("Введите операцию (+, -, *, /): ");
+        char operation = iScanner.next().charAt(0);
+        int result = 0;
+        if (operation == '+') {
+            result = num1 + num2;
+        } else if (operation == '-') {
+            result = num1 - num2;
+        } else if (operation == '*') {
+            result = num1 * num2;
+        } else if (operation == '/') {
+            result = num1 / num2;
+        } else {
+            System.out.println("Такой операции нет!");
+        }
+        System.out.printf(num1 + " " + operation + " " + num2 + " = " + result);
+        iScanner.close();
+    }
 }
